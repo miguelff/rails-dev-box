f70cfb1cb30c20d67ff3be6ade651a1c4de34b98
Mike Fernández <miguelfernandezfernandez@gmail.com>
2016-01-25 09:02:11 +0100

    Set default ruby

--- install-ruby.sh
+++ install-ruby.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "Installing Ruby"
 source /home/vagrant/.rvm/scripts/rvm
-rvm use --default --install $1 >/dev/null 2>&1
+rvm use install $1 >/dev/null 2>&1
+rvm use $1 --default >/dev/null 2>&1
